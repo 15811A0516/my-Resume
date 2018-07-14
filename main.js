4bae03cbccac289f5157a9c55c23e7b3d9b700ba
15811A0516 <chinnipushpalatha516@gmail.com>
2018-07-14 15:10:05 +0530

    hello

---
+++ main.js
@@ -0,0 +1,71 @@
+function loadJson(file,callback)
+{
+var xhr=new XMLHttpRequest();
+xhr.overrideMimeType("application/json");
+xhr.open("GET",file,true);
+xhr.onreadystatechange=function(){
+	if(xhr.readyState===4 && xhr.status=="200"){
+		callback(xhr.responseText);
+	}
+};
+xhr.send();
+}
+loadJson("data.json",function(text){
+	let data=JSON.parse(text);
+	console.log(data);
+	career(data.career);
+	education(data.education);
+	skills(data.skills);
+})
+var childTwo=document.querySelector(".child2");
+function career(careerInfo){
+	var heading=document.createElement("h2");
+	heading.textContent="CAREER OBJECTIVE";
+	childTwo.appendChild(heading);
+	var hr=document.createElement("hr");
+	heading.appendChild(hr);
+	var careerP=document.createElement("p");
+	careerP.textContent=careerInfo.Info;
+	childTwo.appendChild(careerP);
+}
+function education(edu){
+	var heading=document.createElement("h2");
+	heading.textContent="EDUCATIONAL QUALIFICATIONS";
+	childTwo.appendChild(heading);
+	var table=document.createElement("table");
+	childTwo.appendChild(table);
+	table.border="1";
+	var tr1="<tr><td>Degree</td><td>Institute</td><td>Data</td></tr>";
+    var tr2="";
+    for(var i=0;i<edu.length;i++){
+    	tr2+="<tr><td>"+edu[i].Degree+"</td><td>"+edu[i].Institute+"</td><td>"+edu[i].Data+"</td></tr>";
+    }
+
+table.innerHTML=tr1+tr2;
+}
+function skills(skl){
+	var heading=document.createElement("h2");
+	heading.textContent="SKILLS";
+	childTwo.appendChild(heading);
+	var hr=document.createElement("hr");
+	heading.appendChild(hr);
+	for(var i=0;i<skl.length;i++)
+	{
+		var titleH=document.createElement("h3");
+		titleH.textContent=skl[i].title;
+		childTwo.appendChild(titleH);
+	    var ul=document.createElement("ul");
+	    childTwo.appendChild(ul);
+	    for(var j=0;j<skl[i].Info.length;j++){
+	    	var li=document.createElement("li");
+	    	li.textContent=skl[i].Info[j];
+	    	ul.appendChild(li);
+	    }
+
+	}
+
+
+	
+}
+
+
